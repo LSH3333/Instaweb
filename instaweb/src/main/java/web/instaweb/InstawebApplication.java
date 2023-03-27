18729bdca0b76d6093c83406f1e0dfccff6aa18f
LSH3333 <dltpgustpgus@gmail.com>
2023-03-27 13:56:40 +0900

    initial commit

---
+++ instaweb/src/main/java/web/instaweb/InstawebApplication.java
@@ -0,0 +1,13 @@
+package web.instaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstawebApplication.class, args);
+	}
+
+}
---
+++ instaweb/src/test/java/web/instaweb/InstawebApplicationTests.java
@@ -0,0 +1,13 @@
+package web.instaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
