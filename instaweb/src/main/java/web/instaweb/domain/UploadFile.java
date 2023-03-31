6401b8c926a83623fafa626c31e107404f3aeaf4
LSH <dltpgustpgus@gmail.com>
2023-03-31 20:56:38 +0900

    파일 첨부 시스템 작업중 ...

---
+++ instaweb/src/main/java/web/instaweb/domain/UploadFile.java
@@ -0,0 +1,22 @@
+package web.instaweb.domain;
+
+import lombok.Data;
+
+/**
+ * 업로드한 파일 정보
+ */
+
+@Data
+public class UploadFile {
+
+    // 사용자가 업로드한 파일의 이름
+    private String uploadFileName;
+    // 서버에 저장되는 파일의 이름
+    private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+
+}
