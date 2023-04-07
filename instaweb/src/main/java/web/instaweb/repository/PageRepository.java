eed471fcf52b9d300ccfdf3700f3570ae7436e50
LSH3333 <dltpgustpgus@gmail.com>
2023-04-07 15:15:20 +0900

    삭제 테스트 완료, 스크롤 시 리프레쉬 구현 필요

--- instaweb/src/main/java/web/instaweb/repository/PageRepository.java
+++ instaweb/src/main/java/web/instaweb/repository/PageRepository.java
@@ -29,4 +29,14 @@ public class PageRepository {
         Page page = em.find(Page.class, id);
         em.remove(page);
     }
+
+    /**
+     * firstIdx 부터 count 개의 Page 를 가져온다 (생성일 기준 내림차순 정렬)
+     */
+    public List<Page> findPages(int firstIdx, int count) {
+        return em.createQuery("select p from Page p order by p.createdTime desc", Page.class)
+                .setFirstResult(firstIdx)
+                .setMaxResults(count)
+                .getResultList();
+    }
 }
