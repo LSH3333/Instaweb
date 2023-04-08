36e7b50cd3dbc0b2017b6afbe8952077d714df37
LSH <dltpgustpgus@gmail.com>
2023-04-08 20:19:18 +0900

    테스트 데이터들 준비 필요

--- instaweb/src/main/java/web/instaweb/InstawebApplication.java
+++ instaweb/src/main/java/web/instaweb/InstawebApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InstawebApplication {
 
 	public static void main(String[] args) {
+
+
+
 		SpringApplication.run(InstawebApplication.class, args);
 	}
 
