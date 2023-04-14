6a78c57ee9f848c68db48fa77de7fa05eed52b8f
LSH <dltpgustpgus@gmail.com>
2023-04-14 16:08:16 +0900

    수정폼에서 이미지 누를시 선택창 여는것 까지 완료

--- instaweb/src/main/java/web/instaweb/controller/PageController.java
+++ instaweb/src/main/java/web/instaweb/controller/PageController.java
@@ -178,7 +178,7 @@ public class PageController {
     /**
      * 글 수정 폼에서 (updatePageForm.html) 요청
      * 특정 id 페이지에 저장된 모든 이미지들 리턴
-     * 순환참조 오류 때문에 GetImageDto 로 반환 
+     * 순환참조 오류 때문에 GetImageDto 로 반환
      */
     @ResponseBody
     @GetMapping("/pages/requestImages")
