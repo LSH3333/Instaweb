1e4f002a2a5ec84e6ed48f58797c4c9f2a55d65a
LSH3333 <dltpgustpgus@gmail.com>
2023-04-16 12:45:29 +0900

    페이지 수정폼에서 이미지 삭제 구현 중 ...

--- instaweb/src/main/java/web/instaweb/service/ImageService.java
+++ instaweb/src/main/java/web/instaweb/service/ImageService.java
@@ -39,11 +39,20 @@ public class ImageService {
         imageRepository.deleteImage(id);
     }
 
-    // 변경 감지
+    /**
+     * @param id : db 에서 찾을 Image 의 id
+     * @param base64String : base64 string 형식의 이미지 파일
+     *
+     * id 에 해당하는 Image 없으면 return
+     *
+     */
     @Transactional
     public void updateImage(Long id, String base64String) {
         // 여기서 image 는 db에서 가져왔으므로 영속 상태
         Image image = imageRepository.findOne(id);
+        if (image == null) {
+            return;
+        }
         byte[] decoded = Base64.getDecoder().decode(base64String);
         image.changeImage(decoded);
         // @Transactional 에 의해 commit 됨 -> flush (변경 감지)
