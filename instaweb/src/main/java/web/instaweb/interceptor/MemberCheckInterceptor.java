fc57b6646acdcde1b4c6ce9d21a05843301fb898
LSH <dltpgustpgus@gmail.com>
2023-05-12 15:56:07 +0900

    pageBlock 자체를 클릭할수 있도록 수정 완료

--- instaweb/src/main/java/web/instaweb/interceptor/MemberCheckInterceptor.java
+++ instaweb/src/main/java/web/instaweb/interceptor/MemberCheckInterceptor.java
@@ -81,7 +81,6 @@ public class MemberCheckInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HttpSession session = request.getSession();
-        System.out.println("postHandle");
         // 로그인 상태라면 모델에 loginMemberId 추가
         if(session != null && session.getAttribute(SessionConst.LOGIN_MEMBER) != null) {
             Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
